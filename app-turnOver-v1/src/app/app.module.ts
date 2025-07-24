b58014b343e85c06c35d8c09fc77dd901265835a
Bianca Bech <biancapaulabech@gmail.com>
2025-07-24 10:15:34 -0300

    Adicicionado httclient e foi removido imports que não fazia parte do contesto

--- app-turnOver-v1/src/app/app.module.ts
+++ app-turnOver-v1/src/app/app.module.ts
@@ -3,31 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { ContatoComponent } from './components/contato/contato.component';
-import { SobreComponent } from './components/sobre/sobre.component';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { provideHttpClient } from '@angular/common/http';
+import { HttpClientModule, provideHttpClient } from '@angular/common/http';
 import { CadastroFuncionarioComponent } from './components/funcionario/cadastro-funcionario/cadastro-funcionario.component';
 import { MenuComponent } from './components/menu/menu.component';
 import { HomeComponent } from './components/home/home.component';
 import { DesligamentoComponent } from './components/desligamento/desligamento.component';
-import { FitCulturalComponent } from './components/funcionario/fit-cultural/fit-cultural.component';
+import { FitCulturalComponent } from './components/fit-cultural/fit-cultural.component';
 import { GraficoAnualComponent } from './components/dashboard/grafico-anual/grafico-anual.component';
 import { DashboardService } from './services/dashboard.service';
 import { AcompanhamentoComponent } from './components/funcionario/acompanhamento/acompanhamento.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { GraficoComponent } from './components/grafico/grafico.component';
+import { IaIndicaComponent } from './components/ia-indica/ia-indica.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+import { ListagemFuncionariosComponent } from './components/funcionario/listagem-funcionarios/listagem-funcionarios.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     FooterComponent,
-    ContatoComponent,
-    SobreComponent,
     CadastroFuncionarioComponent,
     MenuComponent,
     HomeComponent,
@@ -36,13 +35,20 @@ import { GraficoComponent } from './components/grafico/grafico.component';
     GraficoComponent,
     GraficoAnualComponent,
     AcompanhamentoComponent,
+    IaIndicaComponent,
+    ListagemFuncionariosComponent,
+
   ],
+
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     CommonModule,
     FontAwesomeModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
   ],
   providers: [
     provideHttpClient(),
