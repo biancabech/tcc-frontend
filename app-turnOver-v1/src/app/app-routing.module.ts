aa05a74ebc3aab86086afe0d0740c7bf4d0b87a3
Bianca Bech <biancapaulabech@gmail.com>
2025-08-24 22:41:42 -0300

    Ajustado as URL's

--- app-turnOver-v1/src/app/app-routing.module.ts
+++ app-turnOver-v1/src/app/app-routing.module.ts
@@ -7,13 +7,16 @@ import { HomeComponent } from './components/paginas/home/home.component';
 import { GraficoAnualComponent } from './components/paginas/dashboard/grafico-anual/grafico-anual.component';
 import { FitCulturalComponent } from './components/paginas/paginas-cadastro/cadastro-fit-cultural/fit-cultural.component';
 import { IaIndicaComponent } from './components/paginas/ia-indica/ia-indica.component';
-import { ListagemFuncionariosComponent } from './components/paginas/paginas-listagem/listagem-funcionarios/listagem-funcionarios.component';
 import { CargoComponent } from './components/paginas/paginas-cadastro/cadastro-cargo/cargo.component';
 import { SetorComponent } from './components/paginas/paginas-cadastro/cadastro-setor/setor.component';
-import { ListagemCargoComponent } from './components/paginas/paginas-listagem/listagem-cargos/listagem-cargos.component';
+import { ListagemFuncionariosComponent } from './components/paginas/paginas-listagem/listagem-funcionarios/listagem-funcionarios.component';
 import { ListagemSetoresComponent } from './components/paginas/paginas-listagem/listagem-setores/listagem-setores.component';
 import { ListagemFitCulturalComponent } from './components/paginas/paginas-listagem/listagem-fit-cultural/listagem-fit-cultural.component';
 import { ListagemDesligamentosComponent } from './components/paginas/paginas-listagem/listagem-desligamentos/listagem-desligamentos.component';
+import { ListagemAcompanhamentosComponent } from './components/paginas/paginas-listagem/listagem-acompanhamentos/listagem-acompanhamentos.component';
+import { ListagemCargoComponent } from './components/paginas/paginas-listagem/listagem-cargos/listagem-cargos.component';
+import { ListagemMotivoDesligamentoComponent } from './components/paginas/paginas-listagem/listagem-motivo-desligamento/listagem-motivo-desligamento/listagem-motivo-desligamento.component';
+import { MotivoDesligamentoComponent } from './components/paginas/paginas-cadastro/cadastro-motivo-desligamento/cadastro-motivo-desligamento.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -26,12 +29,15 @@ const routes: Routes = [
   { path: 'paginas/paginas-cadastro/cadastro-cargo', component: CargoComponent },
   { path: 'paginas/paginas-cadastro/cadastro-fit-cultural', component: FitCulturalComponent },
   { path: 'paginas/paginas-cadastro/cadastro-desligamento', component: DesligamentoComponent },
+  { path: 'paginas/paginas-cadastro/cadastro-motivo-desligamento', component: MotivoDesligamentoComponent },
   //Componentes de Listagem
   { path: 'paginas/paginas-listagem/listagem-funcionarios', component: ListagemFuncionariosComponent },
   { path: 'paginas/paginas-listagem/listagem-cargos', component: ListagemCargoComponent },
   { path: 'paginas/paginas-listagem/listagem-setores', component: ListagemSetoresComponent },
   { path: 'paginas/paginas-listagem/listagem-fit-cultural', component: ListagemFitCulturalComponent },
   { path: 'paginas/paginas-listagem/listagem-desligamentos', component: ListagemDesligamentosComponent },
+  { path: 'paginas/paginas-listagem/listagem-acompanhamentos', component: ListagemAcompanhamentosComponent },
+  { path: 'paginas/paginas-listagem/listagem-motivo-desligamento', component: ListagemMotivoDesligamentoComponent },
 ];
 
 @NgModule({
